48c58a2dd211b836c833a274d601e04d31763798
Zhen Data <zhenyang.9261@gmail.com>
2019-12-19 20:13:21 -0500

    Changed config.json to config.js so that we can utilized the process.env variables to protect our local database credentials

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,36 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+CREATE DATABASE Pillow;
+USE Pillow;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE Users(
+	Id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+	-- whatever length of hash is
+    password VARCHAR(255) NOT NULL,
+    firstName VARCHAR(40), 
+    lastName VARCHAR(40), 
+    PRIMARY KEY(Id)
+);
+
+CREATE TABLE Properties(
+	Id INT NOT NULL AUTO_INCREMENT, 
+    street VARCHAR(255) NOT NULL, 
+    city VARCHAR(30) NOT NULL, 
+    stateCode VARCHAR(2), 
+    zipCode VARCHAR(5) NOT NULL, 
+	numBeds DOUBLE NOT NULL, 
+    numBathrooms DOUBLE NOT NULL, 
+    propertyType VARCHAR(25) DEFAULT "house", 
+    numStories INT DEFAULT 1, 
+    sqf INT,
+    yearBuilt INT,
+    price FLOAT,
+    PRIMARY KEY(Id)
+);
+
+CREATE TABLE Users_Properties(
+	userId INT NOT NULL, 
+    propertyId INT NOT NULL, 
+    FOREIGN KEY(userId) REFERENCES Users(Id) ON DELETE CASCADE, 
+    FOREIGN KEY(propertyId) REFERENCES Properties(Id) ON DELETE CASCADE, 
+    PRIMARY KEY(userId, propertyId)
+);
