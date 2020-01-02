9d68240bf2aaa4395c102312318fa50c4ff04264
Peter Connelly <peter@Peters-MacBook-Pro.local>
2020-01-02 13:25:43 -0500

    adding logic for creating new chats

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,117 @@
 var createText = document.querySelector(".chat-typing-form");
+
 const socket = io().connect();
+
 var currChat = document.querySelector(".curr-chat");
 var chatBody = document.querySelector(".chat-body");
+
+var createNewChatButton = document.querySelector(".create-new-chat");
+var chatListContainer = document.querySelector(".chat-list-container");
+
+var currChatInitialsDisplay = document.getElementById("main-chat-initials-display");
+var moreDetailsButton = document.getElementById("more-details-button");
+var chatHeader = document.querySelector(".main-chat-header");
+
+var createNewChatInput = document.querySelector(".create-new-chat-input");
+var createNewChatPrompt = document.querySelector(".create-new-chat-prompt");
 
 var conversations = document.getElementsByClassName("conversation-preview-display");
 var recipientIds = [];
 
-function displayMessages(messages) {
-    messages = JSON.parse(messages);
 
+function clearChatDisplay() {
     while(chatBody.firstChild) {
         chatBody.removeChild(chatBody.firstChild);
     }
+}
+
+function displayMessages(messages) {
+    messages = JSON.parse(messages);
+
+    clearChatDisplay();
 
     for(const message of messages) {
         displayMessage(message.text, message.isFromUser);
     }
+}
+
+function createRecipientInitialsDisplay() {
+    var recipientInitialsDisplayContainer = document.createElement("div");
+    recipientInitialsDisplayContainer.classList.add("recipient-initials-display-container");
+
+    var recipientInitialsDisplay = documne.createElement("div");
+    recipientInitialsDisplay.classList.add("recipient-initials-display");
+
+    var recipientInitials = document.createElement("p");
+    recipientInitials.classList.add("initials");
+    
+    recipientInitialsDisplay.appendChild(recipientInitials);
+    recipientInitialsDisplayContainer.appendChild(recipientInitialsDisplay);
+
+    return recipientInitialsDisplayContainer;
+}
+
+function createRecipientInformationContainer() {
+    var recipientInformationContainer = document.createElement("div");
+
+}
+
+function createNewChatPreviewDisplay() {
+    var newChatDisplay = document.createElement("div");
+    newChatDisplay.classList.add("conversation-preview-display");
+    newChatDisplay.classList("new-conversation")
+    newChatDisplay.classList.add("curr-chat");
+
+    currChat.classList.remove("curr-chat");
+    currChat = newChatDisplay;
+
+    var recipientInitialsDisplay = createRecipientInitialsDisplay();
+    var recipientInformationContainer = createRecipientInformationContainer();
+
+
+    // class="recipient-initials-display-container"
+    // class="recipient-information-container"
+
+    chatListContainer.prepend(currChat);
+}
+
+function createNewChat() {
+    clearChatDisplay();
+    createNewChatPreviewDisplay();
+    
+}
+
+function displayMessage(text, isUserText) {
+    const newText = document.createElement("p");
+    const textContainer = document.createElement("div");
+
+    newText.textContent = text;
+    newText.classList.add("text");
+
+    if(isUserText) {
+        newText.classList.add("user-text");
+        textContainer.classList.add("user-text-container");
+    } else {
+        newText.classList.add("recipient-text");
+        textContainer.classList.add("recipient-text-container");
+    }
+
+    textContainer.appendChild(newText);
+    chatBody.appendChild(textContainer);
+
+    createText.elements[0].value = "";
+}
+
+function hideChatNavDisplay() {
+    currChatInitialsDisplay.style.display = "none";
+    moreDetailsButton.style.display = "none";
+
+    chatHeader.classList.remove("main-chat-header");
+    chatHeader.classList.remove("chat-nav-header");
+    chatHeader.classList.add("create-new-chat-header");
+
+    createNewChatInput.classList.remove("off");
+    createNewChatPrompt.classList.remove("off");
 }
 
 for(const conv of conversations) {
@@ -43,28 +139,6 @@ $.ajax({
 .catch(err => {
     console.log(err);
 })
-
-
-function displayMessage(text, isUserText) {
-    const newText = document.createElement("p");
-    const textContainer = document.createElement("div");
-
-    newText.textContent = text;
-    newText.classList.add("text");
-
-    if(isUserText) {
-        newText.classList.add("user-text");
-        textContainer.classList.add("user-text-container");
-    } else {
-        newText.classList.add("recipient-text");
-        textContainer.classList.add("recipient-text-container");
-    }
-
-    textContainer.appendChild(newText);
-    chatBody.appendChild(textContainer);
-
-    createText.elements[0].value = "";
-}
 
 socket.on("connect", () => {
     $.ajax({
@@ -129,3 +203,8 @@ for(const conversationDisplay of conversations) {
         displayMessages(currChat.dataset['texts']);
     })
 }
+
+createNewChatButton.addEventListener("click", event => {
+    hideChatNavDisplay();
+    createNewChat();
+})
